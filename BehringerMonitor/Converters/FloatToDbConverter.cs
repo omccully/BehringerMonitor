191fb9cd0bc63fa7307ef36824e14d9c1d86b46d
omccully <oscarmccully@gmail.com>
2025-12-28 17:10:39 -0600

    display db indicator

---
+++ BehringerMonitor/Converters/FloatToDbConverter.cs
@@ -0,0 +1,23 @@
+﻿using BehringerMonitor.Helpers;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BehringerMonitor.Converters
+{
+    public class FloatToDbConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is float f)
+            {
+                return DisplayHelper.FloatToDb(f);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
