c93f6ae9189507e10895cb5fe980d2f2cbb5a42b
omccully <oscarmccully@gmail.com>
2026-02-22 15:47:51 -0600

    create helper for parsing folder name

---
+++ BehringerMonitor.Tests/X32BackupHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using BehringerMonitor.Helpers;
+
+namespace BehringerMonitor.Tests
+{
+    public class X32BackupHelperTests
+    {
+        public static IEnumerable<object?[]> GetTestData()
+        {
+            yield return new object?[]
+            {
+                "X32_2026-01-11_11h37",
+                new DateTime(2026, 01, 11, 11, 37, 00)
+            };
+            yield return new object?[]
+            {
+                "X32_2026-02-22_11h53",
+                new DateTime(2026, 02, 22, 11, 53, 00)
+            };
+
+            yield return new object?[]
+            {
+                "Program Files",
+                null
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void ParseFolderDateTime(string folderName, DateTime? expectedDateTime)
+        {
+            DateTime? actual = X32BackupHelper.ParseFolderDateTime(folderName);
+
+            Assert.Equal(expectedDateTime, actual);
+        }
+    }
+}
---
+++ BehringerMonitor/Helpers/X32BackupHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BehringerMonitor.Helpers
+{
+    public static partial class X32BackupHelper
+    {
+        public static DateTime? ParseFolderDateTime(string folderName)
+        {
+            Match m = FolderNameRegex().Match(folderName);
+
+            if (m.Success)
+            {
+                return new DateTime(
+                    int.Parse(m.Groups[1].Value),
+                    int.Parse(m.Groups[2].Value),
+                    int.Parse(m.Groups[3].Value),
+                    int.Parse(m.Groups[4].Value),
+                    int.Parse(m.Groups[5].Value),
+                    0);
+            }
+
+            return null;
+        }
+
+        [GeneratedRegex(@"X32_(\d+)-(\d+)-(\d+)_(\d+)h(\d+)")]
+        private static partial Regex FolderNameRegex();
+    }
+}
